2630bb7f4927d0059848b88724ddef2e3ffa55e7
jcepriano <jamescepriano@gmail.com>
2023-08-30 14:55:55 -0700

    Not Done

--- CommerceAPI/Controllers/MerchantsController.cs
+++ CommerceAPI/Controllers/MerchantsController.cs
@@ -1,7 +1,6 @@
 ﻿using CommerceAPI.DataAccess;
 using CommerceAPI.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace CommerceAPI.Controllers
 {
---
+++ CommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using CommerceAPI.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using CommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceAPI.Controllers
+{
+    [Route("/api/merchants/{merchantId}/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly CommerceApiContext _context;
+
+        public ProductsController(CommerceApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult GetProducts(int merchantId)
+        {
+            var merchantProducts = _context.Products
+                .Where(p => p.Merchant.Id == merchantId)
+                .Include(p => p.Merchant);
+
+
+            return new JsonResult(merchantProducts);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetProduct(int productId)
+        {
+            var product = _context.Products.Find(productId);
+            return new JsonResult(product);
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(int merchantId)
+        {
+            var merchant = _context.Merchants
+                .Where(m => m.Id == merchantId)
+                .Include(m => m.Products)
+                .FirstOrDefault();
+
+            var product = new Product();
+            merchant.Products.Add(product);
+            _context.SaveChanges();
+
+            return new JsonResult(merchant);
+        }
+
+        //[HttpPut("{id}")]
+        //public ActionResult EditProduct(Product product)
+        //{
+            
+        //}
+    }
+}
